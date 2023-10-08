8347142771eea2616b599e651f48c6e1fa188ca4
Malick Gueye <malickgueye@Mac-Pro.local>
2023-10-07 20:14:14 -0500

    DSA graph implemt

--- dsa/dsa/Heap/Heap.java
+++ dsa/dsa/Heap/Heap.java
@@ -1,2 +1,54 @@
-package dsa.Heap;public class Heap {
+package dsa.Heap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Heap {
+     List<Integer> heap;
+
+    public Heap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public Integer parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    public Integer leftChild(int index) {
+        return (index * 2) + 1;
+    }
+
+    public Integer rightChild(int index) {
+        return (index * 2) + 2;
+    }
+
+    public void swapItems(int index1, int index2) {
+        Integer temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+
+    public void insert(Integer newElement) {
+        heap.add(newElement);
+        int newElemtInd =  heap.indexOf(newElement);
+        while(newElemtInd >0) {
+            if(newElement < heap.get(parent(newElemtInd))) {
+                break;
+            }
+            swapItems(newElemtInd, parent(newElemtInd));
+            newElemtInd = parent(newElemtInd);
+
+        }
+    }
+
+    public void remove() {
+    }
+
+    @Override
+    public String toString() {
+        return "Heap{" +
+                "heap=" + heap +
+                '}';
+    }
 }
--- dsa/dsa/Heap/Main.java
+++ dsa/dsa/Heap/Main.java
@@ -1,2 +1,26 @@
-package dsa.Heap;public class Main {
+package dsa.Heap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+
+        heap.insert(44);
+        heap.insert(30);
+        heap.insert(10);
+        heap.insert(50);
+        heap.insert(5);
+        heap.insert(12);
+        heap.insert(3);
+
+        System.out.println(heap);
+
+        heap.remove();
+
+        System.out.println(heap);
+    }
 }
